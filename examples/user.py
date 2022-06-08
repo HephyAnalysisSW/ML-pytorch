d283ee329071d04465e8881d688e7876fd829dfc
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2022-06-08 20:05:19 +0200

    pdf data

--- examples/user.py
+++ examples/user.py
@@ -1,3 +1,5 @@
 import os
 if os.environ['USER'] in ['robert.schoefbeck']:
     plot_directory         = "/groups/hephy/cms/robert.schoefbeck/www/pytorch/"
+elif os.environ['USER'] in ['sridhar.busulu']:
+    plot_directory         = "/groups/hephy/cms/sridhar.busulu/www/pytorch/"
