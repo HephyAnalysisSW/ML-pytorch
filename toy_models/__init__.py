716119e0345941167c7fce0c2da62e1c9d8daa14
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2022-09-21 17:55:34 +0200

    gen

--- toy_models/__init__.py
+++ toy_models/__init__.py
@@ -2,6 +2,6 @@
 #__path__.append(os.path.dirname(os.path.abspath(__file__)))
 import sys, os
 _path = os.path.dirname(os.path.abspath(__file__))
+#print ("insert: %s" %_path )
 sys.path.insert( 0, _path)
 sys.path.insert( 0,'/'.join(_path.split('/')[:-1]))
-
