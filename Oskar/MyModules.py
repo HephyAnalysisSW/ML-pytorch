d7daafd23a17d646be3402752f84d790e14dd9fd
orothb <osker.rothbacher@oeaw.ac.at>
2022-09-29 15:57:53 +0200

    Fixed shape bug

--- Oskar/MyModules.py
+++ Oskar/MyModules.py
@@ -21,6 +21,8 @@ def data_generation(n_data=1000, input_nfeatures = 10, data_range=None, target_f
     x = (upper - lower) * np.random.random_sample((n_data, input_nfeatures)) + lower
     y = target_function(x)
     target_nfeatures = y[0].size
+    print(target_nfeatures)
+    y = y.reshape(n_data, target_nfeatures)    #####not having the correct shape broke the models
 
     x_train = torch.from_numpy(x).float().to(device)
     y_train = torch.from_numpy(y).float().to(device)
@@ -37,14 +39,15 @@ class MyModule(nn.Module):
     # an __init__ function is needed, which has to call (with the super() function) the torch.nn.Module __init__ function
     # input parameters belong in the __init__ method
     # here we define Modules we want to use, eg. linear layers, activation functions, or even some complicated module
-    def __init__(self, input_nfeatures=1, target_nfeatures=1, hidden1=1, hidden2=1):
+    def __init__(self, input_nfeatures=1, target_nfeatures=1, hidden1=1, hidden2=1, activation=nn.ReLU()):
         super().__init__()
-        l1 = nn.Linear(input_nfeatures, hidden1, bias=True)
-        a1 = nn.ReLU()
-        l2 = nn.Linear(hidden1, hidden2, bias=True)
-        a2 = nn.ReLU()
-        l3 = nn.Linear(hidden2, target_nfeatures, bias=True)
+        l1 = nn.Linear(input_nfeatures, hidden1, bias=False)
+        a1 = activation
+        l2 = nn.Linear(hidden1, hidden2, bias=False)
+        a2 = activation
+        l3 = nn.Linear(hidden2, target_nfeatures, bias=False)
         self.module_list = nn.ModuleList([l1,a1,l2,a2,l3])
+
 
     # then we need to implement a forward method that specifies the network structure
     def forward(self, x):
@@ -54,16 +57,34 @@ class MyModule(nn.Module):
         return x
 
 
+    # predict method
+    def predict(self, data):
+        with torch.no_grad():
+            self.eval()
+            return self(data)
 
-def learn(model, x_train, y_train, n_epoch=100, learning_rate=1e-3, plot_every=10, scheduler=False):
-    loss_fn = torch.nn.MSELoss(reduction='sum')
+
+
+# end of class ####################
+
+def learn(model, x_train, y_train, n_epoch=100, learning_rate=1e-3, plot_every=10, scheduler=None):
+
+    def loss_fn(input, target):
+        return ((input-target)**2).mean()
+
+    # loss_fn_2 = torch.nn.MSELoss(reduction='mean')
 
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    if scheduler:
+
+    if scheduler is not None:
         scheduler = StepLR(optimizer=optimizer, step_size=plot_every, gamma=0.9, verbose=False)
 
     losses = []
+    losses_2 = []
+    # gradients = []
+    # learning_rates = []
 
     model.train()
 
@@ -71,38 +92,100 @@ def learn(model, x_train, y_train, n_epoch=100, learning_rate=1e-3, plot_every=1
         # Forward pass: compute predicted y by passing x to the model.
         y_pred = model(x_train)
 
-        # Compute and print loss.
-        loss = loss_fn(y_pred, y_train)
+        # Compute loss.
+        loss = loss_fn(y_train, y_pred)
+        # loss_2 = loss_fn_2(y_train, y_pred)
         losses.append(loss.item())
-        if epoch % plot_every == 0:
-            print(epoch, loss.item(),scheduler.get_last_lr())
+        # losses_2.append(loss_2.item())
+
+        loss_norm = loss_fn(y_train,0)*len(y_train)
+        
 
         # Before the backward pass, use the optimizer object to zero all of the
         # gradients for the variables it will update (which are the learnable
         # weights of the model). This is because by default, gradients are
         # accumulated in buffers( i.e, not overwritten) whenever .backward()
         # is called. Checkout docs of torch.autograd.backward for more details.
-        optimizer.zero_grad()
+        # optimizer.zero_grad()
 
         # Backward pass: compute gradient of the loss with respect to model
         # parameters
         loss.backward()
 
-        # Calling the step function on an Optimizer makes an update to its
-        # parameters
-        optimizer.step()
+        with torch.no_grad():
+            for param in model.parameters():
+                param -= param.grad * learning_rate #/ torch.linalg.norm(param.grad,ord=float('inf'))
 
-        if scheduler:
+            if epoch % plot_every == 0:
+                # for name, param in model.named_parameters():
+                    # if param.requires_grad==True:
+                print(f'epoch: {epoch}, Loss: {loss.item()}, lr: {learning_rate}')
+                        # print(f'{name}: {param.data}')
+                        # print(f'grad: {param.grad}')
+
+            for param in model.parameters():
+                param.grad.zero_()
+
+        # Calling the step function on an Optimizer makes an update to its parameters
+        # optimizer.step()
+
+        if scheduler is not None:
             scheduler.step()
-        #scheduler.get_last_lr()
+            scheduler.get_last_lr()
 
-            
-    plt.plot(losses)
-    plt.savefig(os.path.join( user.plot_directory, "loss.png" ) )
+    # for l in losses:
+        # print(l)
+
+    rel_loss_decrease = np.array(losses[1:])/np.array(losses[:-1])
+    print(rel_loss_decrease.shape)
+
+    fig, axs = plt.subplots(1,2)
+    axs[0].plot(losses)
+    axs[0].set_title('loss')
+    axs[0].set_yscale('log')
+    axs[1].plot(rel_loss_decrease)
+    axs[1].set_yscale('log')
+    axs[1].set_title('relative decrease of loss')
+
+    # plt.plot(losses)
+    # plt.savefig(os.path.join( user.plot_directory, "loss.png" ) )
     plt.show(block=False)
     #plt.pause(2)
     plt.close()
 
-    with torch.no_grad():
-        model.eval()
-        y_train_pred = model(x_train)
+
+def plot_gradients(model, x_train, target_function):
+
+    def loss_fn(input, target):
+        return ((input-target)**2).mean()
+
+    gradients = []
+    x_plot = np.linspace(-1,3,20) # a grid for plotting
+
+    for x in x_plot:
+        for param in model.parameters():
+            param.data.fill_(x)
+
+        y_pred = model(x_train)
+        loss = loss_fn(target_function(x_train), y_pred)
+        loss.backward()
+
+        for param in model.parameters():
+            if param.requires_grad==True:
+                gradients.append(param.grad.item())
+                print(f'x: {x}, loss: {loss}, grad: {param.grad}')
+                param.grad.zero_()
+
+    print(np.array(gradients).flatten())
+
+    plt.plot(x_plot, np.array(gradients).flatten())
+    plt.show()
+
+
+
+def test_kwargs(**kwargs):
+    name = kwargs['name']
+    number = kwargs['number']
+
+    for k,v in kwargs.items():
+        print(f'{k} = {v}')
