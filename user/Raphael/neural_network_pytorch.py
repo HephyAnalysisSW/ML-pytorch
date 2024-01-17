f129b49807191e28fa752effa7598a892a4df3c5
Raphael Gurschl <e11901867@student.tuwien.ac.at>
2024-01-17 10:34:28 +0100

    Fixed Training

--- user/Raphael/neural_network_pytorch.py
+++ user/Raphael/neural_network_pytorch.py
@@ -1,11 +1,12 @@
 #This script for training with a neural network
 
 #Settings
-epochs = 100
+epochs = 30
 
 
 #!/usr/bin/env python
 import ROOT, os
+import sys
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -22,7 +23,6 @@ ROOT.setTDRStyle()
 
 
 if __name__=="__main__":
-    import sys
     sys.path.append('..')
     sys.path.append('../..')
 
@@ -65,123 +65,74 @@ class NeuralNetwork(nn.Module):
         self.output_layer = nn.Linear(n_var_flat+5, 2 if quadratic else 1)
         self.act_output=nn.Sigmoid()
 
-    def forward(self, x):   #is executed every time model(...) is called
+    def forward(self, x, nu):   #is executed every time model(...) is called
         x = self.batch_norm(x)
         x = self.act1(self.fc1(x))  #sigmoid transforms the input data to data between 0 and 1
         x = self.act2(self.fc2(x))
-        x = self.act_output(self.output_layer(x))
-        return x    #returns value between 0 and 1 
+        x = self.act_output(self.output_layer(x))   #returns value between 0 and 1, is equal to 1/(1+r_v)
+        x = 1/nu *torch.log(1/x -1)
+        return x     #Returns Delta Value! 
     
 #Define Loss Function
-def loss(x,y):
-    loss = - (y * torch.log(x) + (1 - y) * torch.log(1 - x))    #Binary Cross Entropy
-    loss = torch.mean(loss)
+def loss(Delta,nu):
+    soft=torch.nn.Softplus(nu)
+    loss = nu*(soft(Delta)+soft(-Delta))
+    loss = torch.sum(loss)
     return loss
 
+def train():
+    generator = data_model.DataGenerator()  # Adjust maxN as needed
+    features, variations = generator[0]
 
-generator = data_model.DataGenerator()  # Adjust maxN as needed
-features, variations = generator[0]
-all_features=features
-all_variations=variations
-"""
-count1=(variations==-2).sum()
-print(count1)
-count2=(variations==-1.5).sum()
-print(count2)
-count3=(variations==-1).sum()
-print(count3)
-count4=(variations==-0.5).sum()
-print(count4)
-count5=(variations==0).sum()
-print(count5)
-count6=(variations==0.5).sum()
-print(count6)
-count7=(variations==1).sum()
-print(count7)
-count8=(variations==1.5).sum()
-print(count8)
-count9=(variations==2).sum()
-print(count9)
-"""
+    sigma_range = np.arange(-2, 2.5, 0.5)
+    loss_matrix = np.zeros((len(sigma_range), epochs))
+    loss_array=[]
+    best_loss=float('inf')
 
-sigma_range = np.arange(-2, 2.5, 0.5)
-loss_matrix = np.zeros((len(sigma_range), epochs))
-ratio_array=[]
-delta_array=[]
-y_pred_array=[]
-
-print("Training starts")
-
-for i,sigma in enumerate(sigma_range):
+    print("Training starts")
     np.random.seed(1)
     torch.manual_seed(1)
-    print("Now training with sigma= " + str(sigma))
-    model = NeuralNetwork(n_var_flat, args.quadratic)   #Reset
+    model = NeuralNetwork(n_var_flat, args.quadratic)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    features=all_features       #Reset
-    variations=all_variations
-    features = features[(variations[:, 0] == sigma) | (variations[:, 0] == 0)] #filter the needed variations and the nominal data
-    variations = variations[(variations[:, 0] == sigma) | (variations[:, 0] == 0)]
-    features_train, features_test, variations_train, variations_test = train_test_split(features, variations) #Seperates the data into a training and a test set
-
-
-    # Training loop
-    features_tensor   = torch.tensor(features, dtype=torch.float32)
-    variations_tensor = torch.tensor(variations, dtype=torch.float32)
-    features_tensor =   torch.where(torch.isnan(features_tensor), torch.tensor(0.0), features_tensor) 
-    variations_tensor  = torch.where(torch.isnan(variations_tensor), torch.tensor(0.0), variations_tensor)
-    variations_tensor   = torch.where(variations_tensor==sigma, torch.tensor(1.0), variations_tensor)   #Replace sigmas with 1 to ensure correct Cross entropy
-
-
-    features_train_tensor   = torch.tensor(features_train, dtype=torch.float32)
-    variations_train_tensor = torch.tensor(variations_train, dtype=torch.float32)
-    features_test_tensor    = torch.tensor(features_test, dtype=torch.float32)
-    variations_test_tensor  = torch.tensor(variations_test, dtype=torch.float32)
-    features_train_tensor    = torch.where(torch.isnan(features_train_tensor), torch.tensor(0.0), features_train_tensor)        #Replaces NaN values in the tensors with 0
-    variations_train_tensor  = torch.where(torch.isnan(variations_train_tensor), torch.tensor(0.0), variations_train_tensor)
-    features_test_tensor     = torch.where(torch.isnan(features_test_tensor), torch.tensor(0.0), features_test_tensor)
-    variations_test_tensor   = torch.where(torch.isnan(variations_test_tensor), torch.tensor(0.0), variations_test_tensor)
-    variations_train_tensor  = torch.where(variations_train_tensor==sigma, torch.tensor(1.0), variations_train_tensor)  #Replace sigmas with 1 to ensure correct Cross entropy
-    variations_test_tensor   = torch.where(variations_test_tensor==sigma, torch.tensor(1.0), variations_test_tensor)
-
-    loss_array=[]
+    print(model)
 
     for epoch in range(epochs):
+        # Training loop
+        Loss = 0
         # Forward pass
-        predictions = model(features_tensor)
-        #print(predictions)
-        # Compute the loss
-        loss_value = loss(predictions, variations_tensor)
-        loss_array.append(loss_value.item())
+        for i,sigma in enumerate(sigma_range):
+            if sigma !=0:
+                selected_features = features[(variations[:, 0] == sigma) | (variations[:, 0] == 0)] #filter the needed variations and the nominal data
+                selected_variations = variations[(variations[:, 0] == sigma) | (variations[:, 0] == 0)]
+                selected_features_tensor   = torch.tensor(selected_features, dtype=torch.float32)
+                selected_variations_tensor = torch.tensor(selected_variations, dtype=torch.float32)
+                selected_features_tensor =   torch.where(torch.isnan(selected_features_tensor), torch.tensor(0.0), selected_features_tensor)   #Replace missing values with 0
+                predictions = model(selected_features_tensor,sigma)
+                #print(predictions)
+                # Compute the loss
+                loss_value = loss(predictions, sigma)
+                Loss+=loss_value.item()
+        if Loss < best_loss:
+            best_loss = Loss
+            best_model = NeuralNetwork(n_var_flat, args.quadratic)
+            best_model.load_state_dict(model.state_dict())
+            best_epoch=epoch       
         #Zero the gradients
         optimizer.zero_grad()
         # Backward pass
         loss_value.backward()
         # Update weights
         optimizer.step()
+        
+        loss_array.append(Loss)
+        print(f'Epoch {epoch+1}/{epochs}, Loss: {Loss}')
+    print("Training finished")
 
-        print(f'Epoch {epoch+1}/{epochs}, Loss: {loss_value.item()}')
+    #Save the data
+    np.savez('training_data.npz', loss_array=loss_array)
+    output_file = 'best_model.pth'
+    torch.save(best_model.state_dict(), output_file)
+    print(f'Best model saved. Best Loss: {best_loss} in Epoch: {best_epoch}')
 
-
-    y_pred=model(features_tensor)
-    y_pred_0=y_pred[variations_tensor==0].detach().numpy()
-    y_pred_array.append(y_pred_0)
-    ratio=(1-y_pred)/(y_pred)
-    #accuracy = (y_pred.round() == variations_tensor).float().mean() #Converts to float and does mean
-    #print("Accuracy: " + str(accuracy.item()))
-    loss_matrix[i, :] = loss_array      #Add the loss array to the matrix
-    ratio_0 = ratio[variations_tensor == 0].detach().numpy()    #Only add the ratios corresponding to the nominal data
-    ratio_array.append(ratio_0)
-    delta=1/sigma*np.log(ratio_0)
-    delta_array.append(delta)
-print("Training finished")
-
-#Save the data
-np.savez('training_data.npz', loss_matrix=loss_matrix, ratio_array=ratio_array, delta_array=delta_array, y_pred_array=y_pred_array, sigma_range=sigma_range, all_features=all_features, all_variations=all_variations)
-
-"""
-# Save the model
-output_file = os.path.join(args.output_directory, 'multiclass_model_pytorch.pth')
-torch.save(model.state_dict(), output_file)
-print('Model saved to {output_file}')
-"""
+if __name__ == "__main__":
+    train()
